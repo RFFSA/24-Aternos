04998ecd850021e8822955b5d263c0fb70e63cf1
RFFSA <118490248+RFFSA@users.noreply.github.com>
2022-11-18 02:32:23 +0200

    Update bot.js

--- JS/bot.js
+++ JS/bot.js
@@ -4,9 +4,9 @@ console.log('Starting...')
 
 function createBot () {
     const bot = mineflayer.createBot({
-    host: "localhost",
-    port: "25565",
-    username: "Test",
+    host: "MoonBoxPvp.aternos.me",
+    port: "57655",
+    username: "xentai24na7",
     version: false
     })
     bot.on('login', function() {
